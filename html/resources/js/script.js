167898cafbb072c9d9e5ab9b19d376c67fa8685b
Simon Hallin <karlerik.simon@outlook.com>
2022-10-22 09:34:52 +0200

    Added new panel with tech info for project card

--- html/resources/js/script.js
+++ html/resources/js/script.js
@@ -10,15 +10,30 @@ let card = (function()
         card.children[0].classList.remove("text-slide");
     }
 
+    function displayTechInfo(panel) {
+        panel.classList.add("panel-slide");
+    }
+
     function onHover(cards)
     {
         [...cards].forEach(card => card.addEventListener("mouseover", function(){ cardHover(card); }));
-        [...cards].forEach(card => card.addEventListener("mouseleave", function(){ cardRemoveHover(card); }))
+        [...cards].forEach(card => card.addEventListener("mouseleave", function(){ cardRemoveHover(card); }));
     }
 
-    return { onHover };
+    function onClick(btns)
+    {
+        let tech_panels = document.getElementsByClassName("projects-item-tech-info");
+        for (let i = 0; i < btns.length; i++)
+        {
+            btns[i].addEventListener("click", function(){ displayTechInfo( tech_panels[i]); });
+        }
+    }
+
+    return { onHover, onClick };
 })();
 
 // global
 let cards = document.getElementsByClassName("projects-item");
+let tech_info_btns = document.getElementsByClassName("tech-info-btn");
 card.onHover(cards);
+card.onClick(tech_info_btns);
