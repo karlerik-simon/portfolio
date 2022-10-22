1a38ff0c9b5190c2429509b34d664c8f92d4e115
Simon Hallin <karlerik.simon@outlook.com>
2022-10-22 08:07:41 +0200

    Added text slide effectto cards

--- html/resources/js/script.js
+++ html/resources/js/script.js
@@ -2,15 +2,21 @@ let card = (function()
 {
     function cardHover(card)
     {
-        
+        card.children[0].classList.add("text-slide");
+    }
+
+    function cardRemoveHover(card)
+    {
+        card.children[0].classList.remove("text-slide");
     }
 
     function onHover(cards)
     {
         [...cards].forEach(card => card.addEventListener("mouseover", function(){ cardHover(card); }));
+        [...cards].forEach(card => card.addEventListener("mouseleave", function(){ cardRemoveHover(card); }))
     }
 
-    return { attachHoverEvent };
+    return { onHover };
 })();
 
 // global
